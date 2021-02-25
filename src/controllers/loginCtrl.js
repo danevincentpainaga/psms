e07df34466bfc5afc7d00fddf16a874806be79a0
danepainaga <danepainaga@gmail.com>
2021-02-25 22:01:20 +0800

    Revised dist

--- src/controllers/loginCtrl.js
+++ src/controllers/loginCtrl.js
@@ -36,9 +36,9 @@ app.controller('loginCtrl',['$scope', '$rootScope', '$cookies', '$window', '$loc
         password: lg.password
       }
 
-      authApiService.getCsrfToken()
-        .then(function(response){
-
+      // authApiService.getCsrfToken()
+      //   .then(function(response){
+        
             authApiService.validateLogin(credentials)
               .then(function(response){
                 let now = new Date();
@@ -53,12 +53,41 @@ app.controller('loginCtrl',['$scope', '$rootScope', '$cookies', '$window', '$loc
                 console.log(err);
             });
 
-      }, function(err){
-        console.log(err);
-      });
+      // }, function(err){
+      //   console.log(err);
+      // });
 
         
     }
   }
 
 }]);
+
+
+// app.factory('XSRFInterceptor', [ function() {
+//     var readCookie = function(name) {
+//         var nameEQ = name + "=";
+//         var ca = document.cookie.split(';');
+//         for(var i=0;i < ca.length;i++) {
+//             var c = ca[i];
+//             while (c.charAt(0)==' ') c = c.substring(1,c.length);
+//             if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+//         }
+//         return null;
+//     }
+
+//     var XSRFInterceptor = {
+//         request: function(config) {
+//             var token = readCookie('XSRF-TOKEN');
+//             if (token) {
+//                 config.headers['X-XSRF-TOKEN'] = token;
+//             }
+//             return config;
+//         }
+//     };
+//     return XSRFInterceptor;
+// }]);
+
+// app.config(['$httpProvider', function($httpProvider) {
+//     $httpProvider.interceptors.push('XSRFInterceptor');
+// }]);
